55626e7801e66b61a46639f38340e51472eefb01
Glocker1s <alexmaxwellaa@gmail.com>
2026-08-12 14:55:48 -0300

    Delete PostgreSQL_Manager directory

--- PostgreSQL_Manager/roles/postgres_manager/files/masking/fc_dbcvm_lab_scramble_database.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE FUNCTION public.fc_dbcvm_lab_scramble_fail()
-RETURNS void
-LANGUAGE plpgsql
-AS $body$
-BEGIN
-  UPDATE tabela_que_nao_existe
-     SET campo = 'teste';
-END;
-$body$;
